cc1af9f81b1db1ef8c64e04394dc0dd61e71fe62
Abdullah <89698334+MJ-02@users.noreply.github.com>
2024-05-21 23:32:58 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,6 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 # Configuration file for the Sphinx documentation builder.
 
 # -- Project information
@@ -17,6 +20,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.napoleon',
 ]
 
 intersphinx_mapping = {
